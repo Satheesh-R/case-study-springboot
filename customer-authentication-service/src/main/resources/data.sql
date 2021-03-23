036fe8364ac88a63ee9e227a7e2cb7ce66929405
Satheesh-R <31403068+Satheesh-R@users.noreply.github.com>
2021-03-23 18:32:13 +0530

    Delete customer-authentication-service directory

--- customer-authentication-service/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-insert into customer_details("CUSTOMER_ID","PASSWORD","FIRSTNAME","LASTNAME") values('Harry','pass123','Harry','Potter');
-insert into customer_details("CUSTOMER_ID","PASSWORD","FIRSTNAME","LASTNAME") values('Hermoine','pass123','Hermoine','Granger');
-insert into customer_details("CUSTOMER_ID","PASSWORD","FIRSTNAME","LASTNAME") values('Ron','pass123','Ron','Weasley');
